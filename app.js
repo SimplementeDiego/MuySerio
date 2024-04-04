7ea3d4de5d2789a4f3be07936ca5722790d0b6d0
SimplementeDiego <102434289+SimplementeDiego@users.noreply.github.com>
2024-04-04 17:23:38 -0300

    arreglos

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 i = 0;
 function Navidad(){
     if(i==0){
-        document.querySelector('div').innerHTML = "Este año navidad cae el 25 de diciembre."
+        document.querySelector('div').innerHTML = "Este año Navidad cae el 25 de diciembre."
         i++;
     }else{
         document.querySelector('div').innerHTML = "Ya te dije cuando cae Navidad, ¿qué más querés?"
