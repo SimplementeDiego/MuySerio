f94ace9642d105726b722314fb9d48afca4f4fe5
SimplementeDiego <102434289+SimplementeDiego@users.noreply.github.com>
2024-04-04 17:48:28 -0300

    mejoras

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 i = 0;
 function Navidad(){
     if(i==0){
-        document.querySelector('div').innerHTML = "Este año Navidad cae el 25 de diciembre."
+        document.querySelector('div').innerHTML = "Este año Navidad cae el 25 de diciembre.";
         i++;
     }else{
-        document.querySelector('div').innerHTML = "Ya te dije cuando cae Navidad, ¿qué más querés?"
+        if(i==1){
+            document.querySelector('div').innerHTML = "Ya te dije cuando cae Navidad, ¿qué más querés?";
+        }
     }
 }
 
+setTimeout( ()=>{
+    if (i==0){
+        document.querySelector('h1').innerHTML = "¿Qué esperás?";
+    }
+}, 10000)
+
+setTimeout( ()=>{
+    if (i==0){
+        document.querySelector('h1').innerHTML = "¡Calculá!";
+    }
+}, 20000)
+
+setTimeout( ()=>{
+    if (i==0){
+        i=2;
+        document.querySelector('h1').innerHTML = "¿Sabes qué? Ya no podés calcular.";
+    }
+}, 30000)
 
