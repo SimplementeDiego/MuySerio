c63bbb83e0477698fff6e0aa4b366e92eca38674
SimplementeDiego <102434289+SimplementeDiego@users.noreply.github.com>
2024-04-04 17:18:13 -0300

    Calculadora de Navidad

---
+++ app.js
@@ -0,0 +1,11 @@
+i = 0;
+function Navidad(){
+    if(i==0){
+        document.querySelector('div').innerHTML = "Este año navidad cae el 25 de diciembre."
+        i++;
+    }else{
+        document.querySelector('div').innerHTML = "Ya te dije cuando cae Navidad, ¿qué más querés?"
+    }
+}
+
+
